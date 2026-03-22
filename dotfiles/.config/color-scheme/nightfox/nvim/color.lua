fddcd9047bed060cc90892b715359f740992f422
Abi39-beep <abimanyur@proton.me>
2026-03-22 10:05:10 +0530

    update
    nvim nightfox color.lua

---
+++ dotfiles/.config/color-scheme/nightfox/nvim/color.lua
@@ -0,0 +1,35 @@
+local set = vim.api.nvim_set_hl
+
+vim.cmd("hi clear")
+
+-- base
+set(0, "Normal", { fg = "#cdcecf", bg = "#192330" })
+set(0, "CursorLine", { bg = "#2b3b51" })
+set(0, "LineNr", { fg = "#575860" })
+set(0, "CursorLineNr", { fg = "#cdcecf" })
+
+-- syntax
+set(0, "Comment", { fg = "#575860", italic = true })
+set(0, "String", { fg = "#81b29a" })
+set(0, "Function", { fg = "#719cd6" })
+set(0, "Keyword", { fg = "#9d79d6" })
+set(0, "Type", { fg = "#dbc074" })
+
+-- diagnostics
+set(0, "DiagnosticError", { fg = "#c94f6d" })
+set(0, "DiagnosticWarn", { fg = "#dbc074" })
+set(0, "DiagnosticInfo", { fg = "#719cd6" })
+set(0, "DiagnosticHint", { fg = "#63cdcf" })
+
+-- tree
+set(0, "NvimTreeNormal", { bg = "#192330" })
+set(0, "NvimTreeFolderName", { fg = "#719cd6" })
+
+-- telescope
+set(0, "TelescopeBorder", { fg = "#393b44" })
+set(0, "TelescopeSelection", { bg = "#2b3b51" })
+
+-- git signs
+set(0, "GitSignsAdd", { fg = "#81b29a" })
+set(0, "GitSignsChange", { fg = "#dbc074" })
+set(0, "GitSignsDelete", { fg = "#c94f6d" })
